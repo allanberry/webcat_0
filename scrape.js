76f8279f58d69af3a7acbff55b6dcc9c0c62cd7d
Allan Berry <allan.berry@gmail.com>
2018-11-23 23:27:11 -0500

    progress.  works

--- scrape.js
+++ scrape.js
@@ -8,144 +8,156 @@ const config = require('./config.json');
 const package = require('./package.json');
 const axios = require('axios');
 
-
+/**
+ * Runs the script.
+ */
 (async function () {
-  const sites = yaml.safeLoad(fs.readFileSync('sites.yaml', 'utf8'));
+  try {
+    const sites = yaml.safeLoad(fs.readFileSync('sites.yaml', 'utf8'));
+    const browser = await puppeteer.launch();
+    for (const site of sites) {
 
+      // convert dates to a more useful format
+      const dates = site.dates
+        ? site.dates.map(date => moment.utc(date))
+        : config.defaultDates.map(date => moment.utc(date));    
 
+      // scrape once for each date
+      for (const date of dates) {
+        await scrape(browser, site, date);
+      }
 
-
-
-  const browser = await puppeteer.launch();
-  for (const site of sites) {
-
-    // convert dates to a more useful format
-    const dates = site.dates.map(date => moment.utc(date));
-
-    // scrape once for each date
-    for (const date of dates) {
-      await scrape(browser, site, date);
+      // finish by scraping current site
+      await scrape(browser, site, moment.utc());
     }
-    // finish by scraping current site
-    await scrape(browser, site, moment.utc());
+    await browser.close();
+  } catch (error) {
+    console.error(error);
   }
-  await browser.close();
 }());
 
-async function scrape(browser, site, date) {
-  const siteDir = `${config.scrapesDir}/${site.slug}`;
-  const token = date.format();
-  const siteMdFile = `${siteDir}/md.yaml`;
-  const currMdFile = `${siteDir}/${token}/md.yaml`;
-  const htmlFile = `${siteDir}/${token}/page.html`;
-  let siteMetadata;
-  let currMetadata;
 
-  // create directory if it doesn't exist
-  const currentDir = `${siteDir}/${token}`;
-  if (!fs.existsSync(currentDir)){
-    util.mkDirByPathSync(currentDir);
-  }
-
-  // setup browser
-  const page = await browser.newPage();
-
-  // convert url, e.g. to wayback machine
-  const retrievalUrl = await getRetrievalUrl(site.url, date);
-
+/**
+ * Scrapes the internet for a site.
+ * @param {browser} browser - A puppeteer browser object.
+ * @param {site} inputSite - A site object.
+ */
+const scrape = async (browser, inputSite, inputDate) => {
   try {
-    // if date is historical (more than a minute ago), access from wayback machine
+    
 
-    // initialize site metadata
-    if (fs.existsSync(siteMdFile)) {
-      // input from file
-      await fs.readFile(siteMdFile, 'utf8', async function(err, contents) {
-        if(err) { console.error(err); }
-        siteMetadata = yaml.safeLoad(contents);
-      });
-    } else {
-      // create from scratch
-      siteMetadata = {
-        slug: site.slug,
-        url: site.url,
-        version: package.version
+    // establish actual site from Wayback Machine
+    const workingSite = inputDate.isBefore(moment().utc(), 'day')
+      ? await getWayback(inputSite.slug, inputSite.url, inputDate)
+      : {
+        slug: inputSite.slug,
+        url: inputSite.url,
+        date: inputDate,
       };
-    }
 
-    // initialize current metadata
-    currMetadata = {
-      urlRetrieved: retrievalUrl,
-      dateArchived: token,
-      dateRetrieved: moment.utc().format(),
-      version: package.version,
-      userAgent: await browser.userAgent(),
-      screenshots: []
-    };
+    // only proceed if a workingSite exists
+    if (workingSite) {
+      // setup browser
+      const page = await browser.newPage();
 
-    // retrieve page
-    await page.goto(retrievalUrl, {
-      waitUntil: 'networkidle0',
-    });
+      // setup variables
+      const siteDir = `${config.scrapesDir}/${workingSite.slug}`;
+      const token = workingSite.date.format();
+      const siteMdFile = `${siteDir}/md.yaml`;
+      const currentDir = `${siteDir}/${token}`;
+      const currMdFile = `${siteDir}/${token}/md.yaml`;
+      const htmlFile =   `${siteDir}/${token}/page.html`;
 
-    // retrieve html
-    // hat tip: https://github.com/GoogleChrome/puppeteer/issues/331#issuecomment-323711582
-    const bodyHtml = await page.evaluate(() => {
-      return new XMLSerializer().serializeToString(document.doctype) + document.documentElement.outerHTML
-    });
-    
-    // load predetermined sizes, and take screenshot
-    for (let key in config.sizes) {
-      const val = config.sizes[key];
-      const filename = `${key}.png`;
-      const imgPath = `${siteDir}/${token}/${filename}`;
-      await page.setViewport(val);
-      // take screenshot
-      await page.screenshot({
-        path: imgPath, // path relative to site root
-        fullPage: true
+      // create directory if it doesn't exist
+      if (!fs.existsSync(currentDir)){
+        util.mkDirByPathSync(currentDir);
+      }
+
+      // initialize site metadata
+      let siteMetadata;
+      if (fs.existsSync(siteMdFile)) {
+        // input from file
+        await fs.readFile(siteMdFile, 'utf8', async function(err, contents) {
+          if(err) { console.error(err); }
+          siteMetadata = yaml.safeLoad(contents);
+        });
+      } else {
+        // create from scratch
+        siteMetadata = {
+          slug: workingSite.slug,
+          url: workingSite.url,
+          version: package.version
+        };
+      }
+
+      // retrieve page
+      await page.goto(workingSite.url, {
+        waitUntil: 'networkidle0',
       });
-      // record metadata about screenshot
-      const dimensions = imgSize(imgPath); // TODO? make async (doesn't seem like a big deal)
-      currMetadata.screenshots.push({
-        filename: filename,
-        width: dimensions.width,
-        height: dimensions.height,
-      })
+
+      // retrieve html
+      // hat tip: https://github.com/GoogleChrome/puppeteer/issues/331#issuecomment-323711582
+      const bodyHtml = await page.evaluate(() => {
+        return new XMLSerializer().serializeToString(document.doctype) + document.documentElement.outerHTML
+      });
+      
+      // load predetermined sizes, and take screenshot
+      let currMetadata = {
+        urlRetrieved: workingSite.url,
+        dateArchived: token,
+        dateRetrieved: moment.utc().format(),
+        version: package.version,
+        userAgent: await browser.userAgent(),
+        screenshots: []
+      };
+      for (let key in config.defaultSizes) {
+        const val = config.defaultSizes[key];
+        const filename = `${key}.png`;
+        const imgPath = `${siteDir}/${token}/${filename}`;
+        await page.setViewport(val);
+        // take screenshot
+        await page.screenshot({
+          path: imgPath, // path relative to site root
+          fullPage: true
+        });
+        // record metadata about screenshot
+        const dimensions = imgSize(imgPath); // TODO? make async (doesn't seem like a big deal)
+        currMetadata.screenshots.push({
+          filename: filename,
+          width: dimensions.width,
+          height: dimensions.height,
+        })
+      }
+      
+      // output metadata
+      await fs.writeFile(siteMdFile, yaml.safeDump(siteMetadata), function(err) {
+        if(err) { console.error(err); }
+      });
+      await fs.writeFile(currMdFile, yaml.safeDump(currMetadata), function(err) {
+        if(err) { console.error(err); }
+      });
+
+      // output HTML
+      await fs.writeFile(htmlFile, bodyHtml, function(err) {
+        if(err) { console.error(err); }
+      });
+
+      // report to console
+      console.log(`OK: ${workingSite.slug} - ${token}`);
+
+      // clean up
+      await page.close();
     }
-    
-    // output metadata
-    await fs.writeFile(siteMdFile, yaml.safeDump(siteMetadata), function(err) {
-      if(err) { console.error(err); }
-    });
-    await fs.writeFile(currMdFile, yaml.safeDump(currMetadata), function(err) {
-      if(err) { console.error(err); }
-    });
-
-    // output HTML
-    await fs.writeFile(htmlFile, bodyHtml, function(err) {
-      if(err) { console.error(err); }
-    });
-
-    // create square tile for general web display
-    // TODO: refactor to crop from another screenshot?
-    // TODO: should not happen on every run
-    await page.setViewport({
-      width: config.tileSize.width,
-      height: config.tileSize.height
-    });
-    await page.screenshot({
-      path: `${siteDir}/tile.png`,
-      fullPage: false
-    });
-  } catch(err) {
-    console.error(`FAILED: ${site.slug} ${token} -- ${err}`)
-  }
-
-  // clean up
-  await page.close();
+  } catch(error) {
+    console.error(error)
+  }  
 }
 
-// generic function to hit a JSON access point
+
+/**
+ * Generic function to hit a JSON access point.
+ * @param {string} url - A url to get data from.
+ */
 const getData = async url => {
   try {
     const response = await axios.get(url);
@@ -155,13 +167,30 @@ const getData = async url => {
   }
 };
 
-const getRetrievalUrl = async (url, date) => {
-  if (date.isBefore(moment.utc(), 'minute')) {
-    // retrieve url from wayback machine
+
+/**
+ * Retrieve workingSite from wayback machine.
+ * @param {string} slug - An identifier.
+ * @param {string} url - A url to get data from the Wayback machine.
+ * @param {moment} date - A moment date object.
+ */
+const getWayback = async (slug, url, date) => {
+  try {
     const waybackProbeUrl = `https://archive.org/wayback/available?url=${url}&timestamp=${date.format('YYYYMMDDHHmmss')}`;
     const waybackResponse = await getData(waybackProbeUrl);
-    return waybackResponse.archived_snapshots.closest.url;
-  } else {
-    return site.url;
+
+    // if waybackResponse.archived_snapshots is empty, abort
+    const obj = waybackResponse.archived_snapshots;
+
+    if (Object.keys(obj).length === 0) {
+      return null;
+    }
+    return { // workingSite object
+      slug: slug,
+      url: waybackResponse.archived_snapshots.closest.url,
+      date: moment.utc(waybackResponse.archived_snapshots.closest.timestamp, 'YYYYMMDDHHmmss'),
+    };
+  } catch (error) {
+    console.error(error);
   }
 }
