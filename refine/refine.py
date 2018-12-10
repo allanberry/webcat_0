d95c64f53e8f052997927e9d4704dd2aa9aa94d4
Allan J. Berry <allan.berry@gmail.com>
2018-12-10 08:11:27 -0600

    output

--- refine/refine.py
+++ refine/refine.py
@@ -1,7 +1,23 @@
 import yaml
+import json
 
-with open("config.yaml", 'r') as stream:
+config_path = 'config.yaml'
+output_path = 'data.json'
+
+# input data
+with open(config_path, 'r') as infile:
     try:
-        print(yaml.load(stream))
+        config_data = yaml.load(infile.read())
     except yaml.YAMLError as err:
         print(err)
+
+organization_names = []
+for s in config_data['sites']:
+    org = s['parent_institution']
+    if org not in organization_names:
+        organization_names.append(org)
+
+# output data
+with open('organizations.json', 'w') as outfile:
+    json.dump(organization_names, outfile)
+
