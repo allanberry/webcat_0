c9c8fc40c5f5e1cad6b0c74655152bb16c3130d3
Allan J. Berry <allan.berry@gmail.com>
2019-01-03 13:03:07 -0600

    ok

--- vue.config.js
+++ vue.config.js
@@ -1,13 +1,15 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
 const clientDir = `${__dirname}/client`;
+const outputDir = `${clientDir}/_dist`;
 
 module.exports = {
   lintOnSave: false,
   configureWebpack: {
 
-    /*
-     * Change 'src' dir to 'client'
-     * https://github.com/vuejs/vue-cli/issues/1134
-     */
+    /**
+      * Change 'src' dir to 'client'
+      * https://github.com/vuejs/vue-cli/issues/1134
+      */
     resolve: {
       alias: {
         '@': clientDir
@@ -16,10 +18,22 @@ module.exports = {
     entry: {
       app: `${clientDir}/main.js`
     },
+
+    plugins: [
+      new CopyWebpackPlugin([
+        /**
+          * Ignore 'public' dir, but copy 'client/public' instead
+          */
+        {
+          from: `${clientDir}/public`,
+          to: outputDir
+        }
+      ], {})
+    ],
   },
 
-  /*
-    * Change 'public' dir to 'client/public'
+  /**
+    * Move 'index.html' to client dir
     * https://stackoverflow.com/a/49437325/652626
     */
   chainWebpack: config => {
@@ -31,9 +45,9 @@ module.exports = {
       })
   },
 
-  /*
-   * Change 'dist' dir to 'client/dist'
-   * https://github.com/vuejs/vue-cli/issues/1496#issuecomment-410546250
-   */
-  outputDir: `${clientDir}/_dist`,
+  /**
+    * Change 'dist' dir to 'client/dist'
+    * https://github.com/vuejs/vue-cli/issues/1496#issuecomment-410546250
+    */
+  outputDir: outputDir
 };
