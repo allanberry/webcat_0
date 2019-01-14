84abb906347f31ec2c479cafedd898d306134227
Allan J. Berry <allan.berry@gmail.com>
2019-01-14 06:58:54 -0600

    progress

--- get_scrapes.py
+++ get_scrapes.py
@@ -3,6 +3,7 @@ from googleapiclient import discovery
 from httplib2 import Http
 from oauth2client import file, client, tools
 from pprint import pprint
+from requests import requests
 
 # If modifying these scopes, delete the file token.json.
 SCOPES = "https://www.googleapis.com/auth/spreadsheets"
@@ -30,23 +31,8 @@ def lists_to_dicts(lists):
     return output
 
 
-def main():
-    """Shows basic usage of the Sheets API.
-    Prints values from a sample spreadsheet.
-    """
-    # The file token.json stores the user's access and refresh tokens, and is
-    # created automatically when the authorization flow completes for the first
-    # time.
-    store = file.Storage("token.json")
-    creds = store.get()
-    if not creds or creds.invalid:
-        flow = client.flow_from_clientsecrets("credentials.json", SCOPES)
-        creds = tools.run_flow(flow, store)
-
-    service = discovery.build("sheets", "v4", http=creds.authorize(Http()))
-
-    # Call the Sheets API
-    libraries = lists_to_dicts(
+def download_libraries(service):
+    return lists_to_dicts(
         (
             service.spreadsheets()
             .values()
@@ -56,16 +42,18 @@ def main():
         )
     )
 
+
+def upload_visit(service, visit):
     value_range_body = {
         "values": [
             [
-                "id",
-                "library",
-                "url",
-                "wayback",
-                "date_retrieved",
-                "date_archived",
-                "source",
+                visit["id"],
+                visit["library"],
+                visit["url"],
+                visit["wayback"],
+                visit["date_retrieved"],
+                visit["date_archived"],
+                visit["source"],
             ]
         ],
         "majorDimension": "ROWS",
@@ -86,7 +74,38 @@ def main():
     response = request.execute()
 
     # # TODO: Change code below to process the `response` dict:
-    # pprint(response)
+    # return response
+
+
+def main():
+    """Shows basic usage of the Sheets API.
+    Prints values from a sample spreadsheet.
+    """
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    store = file.Storage("token.json")
+    creds = store.get()
+    if not creds or creds.invalid:
+        flow = client.flow_from_clientsecrets("credentials.json", SCOPES)
+        creds = tools.run_flow(flow, store)
+
+    service = discovery.build("sheets", "v4", http=creds.authorize(Http()))
+
+    # Call the Sheets API
+    libraries = download_libraries(service)
+
+    visit = {
+        "id": "asdf",
+        "library": "asdf",
+        "url": "asdf",
+        "wayback": "asdf",
+        "date_retrieved": "asdf",
+        "date_archived": "asdf",
+        "source": "asdf",
+    }
+
+    upload_visit(service, visit)
 
 
 if __name__ == "__main__":
