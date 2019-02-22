02e2e26d35f52477129e26415324a1d23d3191a2
Allan Berry <allan.berry@gmail.com>
2019-02-22 11:03:23 -0600

    works, slow as hell

--- visit.py
+++ visit.py
@@ -3,17 +3,51 @@ import dataset
 import arrow
 import csv
 import asyncio
+import os
 from pyppeteer import launch
 
 
 # config
-initial_date = '1995-01-01'
+initial_date = '2017-01-01'
 
 # setup db
 db = dataset.connect("sqlite:///data/webcat.db")
 
 
-def scrape_wayback(site, date_input):
+async def get_screenshots(site, date_short):
+    # setup puppeteer
+    browser = await launch()
+
+    page = await browser.newPage()
+
+    url = f'https://web.archive.org/web/{date_short}/{site["url"]}'
+    await page.goto(url)
+        # wait_until('networkidle0')
+
+    await page.evaluate('''() => {
+                let dom = document.querySelector('#wm-ipp');
+                dom.parentNode.removeChild(dom);
+            }''')
+
+    defaultSizes = {
+            "0600x0": {"width": 600, "height": 0},
+            "1200x0": {"width": 1200, "height": 0}}
+
+    for key, value in defaultSizes.items():
+        await page.setViewport(value)
+
+        screenshot_path = f'data/screenshots/{site["slug"]}/{date_short}/'
+        os.makedirs(screenshot_path, exist_ok=True)
+
+        await page.screenshot({'path': f'{screenshot_path}/{key}.png'})
+
+    
+    # tear down puppeteer
+    await browser.close()
+
+
+# async def scrape_wayback(browser, site, date_input):
+async def scrape_wayback(site, date_input):
     # get correct date
     wb_format = "YYYYMMDDHHmmss"
     date_wanted = date_input.format(wb_format)
@@ -40,13 +74,17 @@ def scrape_wayback(site, date_input):
         table = db['scrapes']
         table.insert({
             'slug': site['slug'],
-            'url': site['url'],
-            'url_requested': wayback_url,
-            'content_type': r.headers['content-type'],
-            'encoding': r.encoding,
-            'text': r.text,
-            'date_scrape': date.to('utc').isoformat()
+            'url_site': site['url'],
+            'url_wayback': wayback_url,
+            'response_content_type': r.headers['content-type'],
+            'response_encoding': r.encoding,
+            'response_text_utf8': r.text,
+            'date_wayback': date.to('utc').isoformat(), 
+            'date_retrieved': arrow.now().isoformat()
         })
+
+        # get screenshot
+        await get_screenshots(site, date_wb)
 
         # report to screen
         print(f'OK: {site["slug"]} {date_short}')
@@ -56,47 +94,31 @@ def scrape_wayback(site, date_input):
 
 
 async def main():
+
+
+
     # setup site
+    with open("data/libraries.csv") as libraries_csv:
+        reader = csv.reader(libraries_csv)
 
-    # with open("data/libraries.csv") as libraries_csv:
-    #     reader = csv.reader(libraries_csv)
+        cols = next(reader)
+        for row in reader:
+            # setup date
+            date = arrow.get(initial_date)
 
-    #     # setup date
-    #     date = arrow.get(initial_date)
-
-    #     cols = next(reader)
-    #     for row in reader:
-    #         site = {}
-    #         for index, col in enumerate(row):
-    #             site[cols[index]] = col
+            # setup site
+            site = {}
+            for col_index, col in enumerate(row):
+                col_name = cols[col_index]
+                site[col_name] = col
             
-    #         while date <= arrow.utcnow():
-    #             scrape_wayback(site, date)
-    #             date = date.shift(years=+1)
-
-    browser = await launch()
-    page = await browser.newPage()
-
-    url = 'https://web.archive.org/web/20090720015749/http://library.uic.edu/'
-    await page.goto(url)
-        # wait_until('networkidle0')
-
-    defaultSizes = {
-            "0600x0": {"width": 600, "height": 0},
-            "1200x0": {"width": 1200, "height": 0}}
-
-    await page.setViewport({"width": 200, "height": 0})
+            # iterate sites/dates
+            while date <= arrow.utcnow():
+                # await scrape_wayback(browser, site, date)
+                await scrape_wayback(site, date)
+                date = date.shift(years=+1)
 
 
-    await page.evaluate('''() => {
-            let dom = document.querySelector('#wm-ipp');
-            dom.parentNode.removeChild(dom);
-        }''')
-
-
-    await page.screenshot({'path': 'data/screenshots/example.png'})
-    await browser.close()
-            
             
 
 
