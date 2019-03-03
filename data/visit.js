3bb4935c6f46d59cfc3632e0a3154fbbf265bb41
Allan Berry <allan.berry@gmail.com>
2019-03-03 00:03:16 -0600

    refactoring, adding, blah

--- data/visit.js
+++ data/visit.js
@@ -5,17 +5,40 @@ const csvParse = require("csv-parse/lib/sync");
 const puppeteer = require("puppeteer");
 const log4js = require("log4js");
 const args = require("minimist")(process.argv.slice(2));
+const Datastore = require("nedb");
+const geoip = require("geoip-lite");
+const slugify = require("slugify");
+const pjson = require('../package.json');
 
 // config
+const scrapesDir = "data/scrapes";
+const waybackDateFormat = "YYYYMMDDHHmmss";
 // const url = args.url;
 const startDate = args.startDate ? args.startDate : moment.utc("1995-01-01");
 const endDate = args.endDate ? args.endDate : moment();
 // const increment = args.increment ? args.increment : "1 year";
 
-const screenshotsDir = "data/scrapes/screens";
-const wbFormat = "YYYYMMDDHHmmss";
+// setup database
+const db = new Datastore({ filename: "data/nedb.db", autoload: true });
 
+// setup logger
 const logger = setupLogger();
+
+// puppeteer viewports, for screenshots
+const viewports = [
+  {
+    name: "mobile",
+    width: 600,
+    height: 1,
+    isLandscape: false
+  },
+  {
+    name: "desktop",
+    width: 1200,
+    height: 1,
+    isLandscape: true
+  }
+];
 
 /**
  * Main process.
@@ -25,19 +48,34 @@ const logger = setupLogger();
     // setup
     const browser = await puppeteer.launch();
     const libraries = await readCSV("data/libraries.csv");
+    const pages = await readCSV("data/pages.csv");
+
+    // get global ip address
+    const response = await request.get("ipv4bot.whatismyipaddress.com");
+    const ip = response.text;
 
     // iterate libraries
-    for (const library of libraries) {
+    for (const page of pages) {
       // iterate dates
       let date = startDate;
-      // while (date.isBefore(endDate)) {
-      // scrape
-      const wb = await scrapeWayback(library.slug, library.url, date, browser);
+      while (date.isBefore(endDate)) {
+        // scrape
+        const wb = await scrapeWayback(page.url, date, browser);
 
-      // console.log(wb);
+        // include ip address and location information
+        wb.source = {
+          ip: ip,
+          geo: geoip.lookup(ip)
+        };
 
-      // date = date.add(1, "years");
-      // }
+        db.insert(wb, function(err, newDoc) {
+          // Callback is optional
+          // newDoc is the newly inserted document, including its _id
+          // newDoc has no key called notToBeSaved since its value was undefined
+        });
+
+        date = date.add(1, "years");
+      }
     }
 
     // cleanup
@@ -53,9 +91,13 @@ const logger = setupLogger();
  * @param {date} dateRequested - A moment date.
  * @param {browser} browser - A Puppeteer browser object.
  */
-async function scrapeWayback(slug, url, date, browser) {
+async function scrapeWayback(url, date, browser) {
   // date format string for moment
-  const wbFormat = "YYYYMMDDHHmmss";
+  const waybackDateFormat = "YYYYMMDDHHmmss";
+  const slug = slugify(url)
+    .replace("/", "_")
+    .replace("https:", "")
+    .replace("http:", "");
 
   try {
     // setup puppeteer
@@ -80,20 +122,46 @@ async function scrapeWayback(slug, url, date, browser) {
     });
 
     // puppeteer take screenshots
-    await screenshots(page, screenshotsDir, slug, dateActual);
+    let screenshots = [];
+    for (const viewport of viewports) {
+      screenshots.push(
+        await screenshot(
+          page,
+          `${scrapesDir}/screens/${slug}`,
+          dateActual,
+          viewport
+        )
+      );
+    }
 
-    // retrieve raw archive HTML from superagent
+    // retrieve raw archive HTML from superagent, and output to file
     const rawHtml = await request.get(rawUrl);
+    const pageDir = `${scrapesDir}/pages/${slug}`;
+    await fs.promises.mkdir(pageDir, { recursive: true });
+    const pageName = `${dateActual.format(waybackDateFormat)}.html`
+    const pageFile = `${pageDir}/${pageName}`;
+    if (!fs.existsSync(pageFile)) {
+      await fs.promises.writeFile(pageFile, rawHtml.text);
+      logger.info(`page OK:   ${pageFile}`);
+    } else {
+      logger.warn(`page --:   ${pageFile} (exists)`);
+    }
 
     // output
     const output = {
       url: url,
+      slug: slug,
       date: dateActual.format(),
       dateScraped: moment.utc().format(),
 
       // for data from puppeteer, for the most part
       rendered: {
         url: wbUrl,
+        agent: {
+          name: 'Node.js/Puppeteer',
+          url: 'https://github.com/GoogleChrome/puppeteer',
+          version: pjson.dependencies.puppeteer
+        },
         page: {
           metrics: await page.metrics(),
           title: await page.title()
@@ -101,47 +169,31 @@ async function scrapeWayback(slug, url, date, browser) {
         browser: {
           userAgent: await browser.userAgent(),
           version: await browser.version()
-        }
+        },
+        screenshots: screenshots
       },
 
       // for raw HTML, from Superagent
       raw: {
         url: rawUrl,
+        agent: {
+          name: 'Node.js/Superagent',
+          url: 'https://github.com/visionmedia/superagent',
+          version: pjson.dependencies.superagent
+        },
         response: {
           status: rawHtml.status,
           type: rawHtml.type,
           headers: rawHtml.header
-        }
+        },
+        html: `${pageName}`
       }
     };
 
     // clean up
     await page.close();
 
-    // // output metadata
-    // const mdFile = `${metaDir}/${library.slug}-${dateActual.format(wbFormat)}.json`;
-    // if (!fs.existsSync(mdFile)) {
-    //   await fs.promises.writeFile(mdFile, JSON.stringify(output));
-    //   logger.info(`meta OK:   ${library.slug}-${dateActual.format(wbFormat)}.json`);
-    // } else {
-    //   logger.warn(
-    //     `meta --:   ${library.slug}-${dateActual.format(wbFormat)}.json (exists)`
-    //   );
-    // }
-
-    // // output raw html
-    // const pageFile = `${pagesDir}/${library.slug}-${dateActual.format(
-    //   wbFormat
-    // )}.html`;
-    // if (!fs.existsSync(pageFile)) {
-    //   await fs.promises.writeFile(pageFile, waybackRaw.text);
-    //   logger.info(`page OK:   ${library.slug}-${dateActual.format(wbFormat)}.html`);
-    // } else {
-    //   logger.warn(
-    //     `page --:   ${library.slug}-${dateActual.format(wbFormat)}.html (exists)`
-    //   );
-    // }
-
+    // finally return output
     return output;
   } catch (error) {
     logger.error(error);
@@ -156,8 +208,8 @@ async function scrapeWayback(slug, url, date, browser) {
  */
 function waybackUrl(url, date, raw = false) {
   return raw
-    ? `http://web.archive.org/web/${date.format(wbFormat)}id_/${url}`
-    : `http://web.archive.org/web/${date.format(wbFormat)}/${url}`;
+    ? `http://web.archive.org/web/${date.format(waybackDateFormat)}id_/${url}`
+    : `http://web.archive.org/web/${date.format(waybackDateFormat)}/${url}`;
 }
 
 /**
@@ -169,13 +221,13 @@ async function getWaybackAvailableDate(url, date) {
   // inquire with wayback for archived site closest in time to input date
   const availableResponse = await request.get(
     `https://archive.org/wayback/available?url=${url}/&timestamp=${date.format(
-      wbFormat
+      waybackDateFormat
     )}`
   );
   // determine date and actual Wayback URLs from superagent
   return moment.utc(
     availableResponse.body.archived_snapshots.closest.timestamp,
-    wbFormat
+    waybackDateFormat
   );
 }
 
@@ -210,157 +262,47 @@ async function readCSV(csv) {
 }
 
 /**
- * Get screenshots from puppeteer
+ * Get screenshot from puppeteer
  * @param {page} page - A puppeteer page
- * @param {path} path - a local directory path
+ * @param {string} dir - a local directory path
  * @param {string} slug - an arbitrary identifier
- * @param {string} date - A moment date.
+ * @param {object} viewport
  */
-async function screenshots(page, path, slug, date) {
-  const viewports = {
-    // "0001x0001": {
-    //   width: 1,
-    //   height: 1,
-    //   // isMobile: false,
-    //   isLandscape: false
-    // },
-    "0600": {
-      width: 600,
-      height: 1
-      // isMobile: true,
-      // isLandscape: false
-    },
-    "1200": {
-      width: 1200,
-      height: 1
-      // isMobile: false,
-      // isLandscape: true
-    }
-  };
-
+async function screenshot(page, dir, date, viewport) {
+  const name = `${date.format(waybackDateFormat)}-${viewport.name}.png`;
+  const file = `${dir}/${name}`;
   try {
     // setup screenshot directory
-    await fs.promises.mkdir(path, { recursive: true });
+    await fs.promises.mkdir(dir, { recursive: true });
+    await page.setViewport(viewport);
 
-    for (const v in viewports) {
-      await page.setViewport(viewports[v]);
-
-      const screenFile = `${slug}-${date.format(wbFormat)}-${v}.png`;
-      const screenPath = `${path}/${screenFile}`;
-
-      // determine if pic already exists
-      if (!fs.existsSync(screenPath)) {
-        await page.screenshot({
-          path: screenPath,
-          fullPage: true,
-          omitBackground: true
-        });
-        logger.info(`screenshot OK: ${screenFile}`);
-      } else {
-        logger.warn(`screenshot --: ${screenFile} (exists)`);
-      }
+    // determine if pic already exists
+    if (!fs.existsSync(file)) {
+      await page.screenshot({
+        path: file,
+        fullPage: viewport.height <= 1 ? true : false
+        // omitBackground: true
+      });
+      logger.info(`screen OK: ${file}`);
+    } else {
+      logger.warn(`screen --: ${file} (exists)`);
     }
-  } catch (error) {
-    logger.error(`screenshot !!: ${slug}-${date.format(wbFormat)}`);
-    logger.error(error);
-  }
-}
 
-/**
- * Get data from Puppeteer
- */
-async function getPageData(page, url) {
-  // setup filesystem
-  // const metaDir = `data/scrapes/meta`;
-  // const pagesDir = `data/scrapes/pages`;
-  // const screensDir = `data/scrapes/screens`;
-  // await fs.promises.mkdir(metaDir, { recursive: true });
-  // await fs.promises.mkdir(pagesDir, { recursive: true });
-  // await fs.promises.mkdir(screensDir, { recursive: true });
-
-  // puppeteer
-  try {
-    // const viewports = {
-    //   // "0001x0001": {
-    //   //   width: 1,
-    //   //   height: 1,
-    //   //   // isMobile: false,
-    //   //   isLandscape: false
-    //   // },
-    //   "0600": {
-    //     width: 600,
-    //     height: 1
-    //     // isMobile: true,
-    //     // isLandscape: false
-    //   },
-    //   "1200": {
-    //     width: 1200,
-    //     height: 1
-    //     // isMobile: false,
-    //     // isLandscape: true
-    //   }
-    // };
-
-    // scraping
-    // for (const v in viewports) {
-
-    // Navigate to page
-    // await page.goto(url, {
-    //   waitUntil: "networkidle0"
-    //   // timeout: 100000
-    // });
-
-    // // strip wayback div
-    // await page.evaluate(() => {
-    //   let waybackDiv = document.querySelector("#wm-ipp");
-    //   waybackDiv.parentNode.removeChild(waybackDiv);
-    // });
-    // await page.setViewport(viewports[v]);
-
-    // // take screenshots
-    // const screenFile = `${screensDir}/${library.slug}-${date.format(
-    //   wbFormat
-    // )}-${v}.png`;
-    // if (!fs.existsSync(screenFile)) {
-    //   await page.screenshot({
-    //     path: screenFile,
-    //     fullPage: true,
-    //     omitBackground: true
-    //   });
-    //   logger.info(
-    //     `screen OK: ${library.slug}-${date.format(wbFormat)}-${v}.png`
-    //   );
-    // } else {
-    //   logger.warn(
-    //     `screen --: ${library.slug}-${date.format(
-    //       wbFormat
-    //     )}-${v}.png (exists)`
-    //   );
-    // }
+    // var _docHeight = (document.height !== undefined) ? document.height : document.body.offsetHeight;
+    // var _docWidth = (document.width !== undefined) ? document.width : document.body.offsetWidth;
 
     return {
-      metrics: await page.metrics(),
-      title: await page.title()
-      // browser: {
-      //   userAgent: await browser.userAgent(),
-      //   version: await browser.version()
-      // }
+      name: name,
+      requestedViewport: viewport,
+      dimensionsTotal: {
+        height: 123,
+        width: 789,
+      }
     };
-
-    // }
   } catch (error) {
-    // logger.error(
-    //   `screen FAIL: ${library.slug}-${date.format(wbFormat)} error: ${
-    //     error.name
-    //   } url: ${waybackUrl}`
-    // );
-    // logger.error(error);
-    // console.log(
-    //   `${colors.red("scrn NO")}: ${date.format(wbFormat)} ${
-    //     library.slug
-    //   } (${colors.red(error.name)})`
-    //   // error
-    // );
+    logger.error(`screen !!: ${file}`);
     logger.error(error);
   }
 }
+
+async function getHtml(dir, date) {}
