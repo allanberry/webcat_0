962e903ec426172fce376ba00f93da8b5d24211e
Allan Berry <allan.berry@gmail.com>
2019-03-13 22:50:12 -0500

    cleanup

--- server/app.js
+++ server/app.js
@@ -1,14 +1,5 @@
-const Koa = require('koa');
-// const app = new Koa();
-
-// app.use(async ctx => {
-//   ctx.body = 'Hello Blarm';
-// });
-
-// app.listen(3000);
-
-
-const { ApolloServer, gql } = require('apollo-server-koa');
+const Koa = require("koa");
+const { ApolloServer, gql } = require("apollo-server-koa");
 
 // Construct a schema, using GraphQL schema language
 const typeDefs = gql`
@@ -20,15 +11,20 @@ const typeDefs = gql`
 // Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'Hello world!',
-  },
+    hello: () => "Hello world!"
+  }
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
-
 const app = new Koa();
+
 server.applyMiddleware({ app });
 
+// browser response
+app.use(async ctx => {
+  ctx.body = "Hello Blarm";
+});
+
 app.listen({ port: 3000 }, () =>
-  console.log(`🚀 Server ready at http://localhost:3000${server.graphqlPath}`),
+  console.log(`🚀 Server ready at http://localhost:3000`)
 );
